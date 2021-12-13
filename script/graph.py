23e22a657a892293479e24ece178d73fb1a72846
marcocuccarini <59500902+marcocuccarini@users.noreply.github.com>
2021-12-13 15:18:34 +0100

    Update graph.py

--- script/graph.py
+++ script/graph.py
@@ -17,6 +17,19 @@ from scipy.spatial import distance
 from sklearn.cluster import KMeans
 
 
+def find_weidth_shortest_path(graph, start, end, path =[]):
+		path = path + [start]
+		if start == end:
+			return path
+		shortest = None
+		for node in graph[start]:
+				if node not in path:
+					newpath = find_shortest_path(graph, node, end, path)
+					if newpath:
+						if not shortest or len(newpath) < len(shortest):
+							shortest = newpath
+		return shortest
+
 class graph():
 	def __init__(self,gdict=None):
 		if gdict is None:
@@ -43,18 +56,7 @@ class graph():
 		return edgename
 
 
-	def find_weidth_shortest_path(graph, start, end, path =[]):
-		path = path + [start]
-		if start == end:
-			return path
-		shortest = None
-		for node in graph[start]:
-				if node not in path:
-					newpath = find_shortest_path(graph, node, end, path)
-					if newpath:
-						if not shortest or len(newpath) < len(shortest):
-							shortest = newpath
-		return shortest
+	
 
 
 
