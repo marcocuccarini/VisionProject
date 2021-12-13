2e84d538cb37d5b37cda892934e58cffae440c4a
marcocuccarini <59500902+marcocuccarini@users.noreply.github.com>
2021-12-13 14:41:03 +0100

    Update graph.py

--- script/graph.py
+++ script/graph.py
@@ -43,6 +43,20 @@ class graph():
 		return edgename
 
 
+	def find_weidth_shortest_path(graph, start, end, path =[]):
+		path = path + [start]
+		if start == end:
+			return path
+		shortest = None
+		for node in graph[start]:
+		if node not in path:
+			newpath = find_shortest_path(graph, node, end, path)
+				if newpath:
+					if not shortest or len(newpath) < len(shortest):
+						shortest = newpath
+		return shortest
+
+
 
 
 	 
