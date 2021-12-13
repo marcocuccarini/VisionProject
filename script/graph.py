06f4537dd9236d2b0e6930126d79e5ba34cae7dc
marcocuccarini <59500902+marcocuccarini@users.noreply.github.com>
2021-12-13 14:43:59 +0100

    Update graph.py

--- script/graph.py
+++ script/graph.py
@@ -49,11 +49,11 @@ class graph():
 			return path
 		shortest = None
 		for node in graph[start]:
-		if node not in path:
-			newpath = find_shortest_path(graph, node, end, path)
-				if newpath:
-					if not shortest or len(newpath) < len(shortest):
-						shortest = newpath
+				if node not in path:
+					newpath = find_shortest_path(graph, node, end, path)
+						if newpath:
+							if not shortest or len(newpath) < len(shortest):
+								shortest = newpath
 		return shortest
 
 
