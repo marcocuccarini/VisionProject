bdbb12adf0c1dde22dff945e85f2afd74edcf79e
marcocuccarini <59500902+marcocuccarini@users.noreply.github.com>
2021-12-15 05:07:58 +0100

    Update utility.py

--- script/utility.py
+++ script/utility.py
@@ -18,7 +18,7 @@ def dfs1(start, target, path, visited = set()):
 
 def d(start,target):
     sumW=0
-    res=dfs1(start, target, path)
+    res=dfs1(start, target, path=[])
     for i in range(len(res)-1):
             sumW+= res[i].get_weight(res[i+1])
 
