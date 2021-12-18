bcd7c7e0f3d805dea02bd896d2204ce0cca6c4b2
marcocuccarini <59500902+marcocuccarini@users.noreply.github.com>
2021-12-18 13:53:38 +0100

    Update __init__.py

--- script/__init__.py
+++ script/__init__.py
@@ -47,9 +47,9 @@ class BagofWord():
 			            features.append(des)
 			        sift_vectors[key] = features
 			    descriptor_list = asarray(descriptor_list)
-				sift_vectors = asarray(sift_vectors)
-				savetxt('/content/drive/MyDrive/dataset2/descriptor_list.csv', descriptor_list, delimiter=',')
-				savetxt('/content/drive/MyDrive/dataset2/sift_vectors.csv', sift_vectors, delimiter=',')
+			    sift_vectors = asarray(sift_vectors)
+			    savetxt('/content/drive/MyDrive/dataset2/descriptor_list.csv', descriptor_list, delimiter=',')
+			    savetxt('/content/drive/MyDrive/dataset2/sift_vectors.csv', sift_vectors, delimiter=',')
 			    #return [descriptor_list, sift_vectors]
 
         #cerca il cetroide e lo associa al cluster
