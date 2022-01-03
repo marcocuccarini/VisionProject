3a68e9be137220b510fad8b1db690d5f927a9fb5
marcocuccarini <59500902+marcocuccarini@users.noreply.github.com>
2022-01-03 17:39:01 +0100

    Update preprocessing.py

--- script/preprocessing.py
+++ script/preprocessing.py
@@ -141,7 +141,7 @@ class Preprocessing:
     def SSE(self, dictFlat):
         dictSSE={}
         for j in dictFlat.keys():
-            listFlat=dictFlat
+            listFlat=dictFlat[j]
             kmeans = KMeans(n_clusters=(1), random_state=0).fit(listFlat)
             dictSSE[kmeans.inertia_]=j
 
