cb3d17ba590b7772fb7cd37f6aeded6c525a3b96
marcocuccarini <59500902+marcocuccarini@users.noreply.github.com>
2022-01-07 22:32:20 +0100

    Update __init__.py

--- script/__init__.py
+++ script/__init__.py
@@ -47,8 +47,8 @@ class BagofWord():
 				features = []
 				for img in value:
 					W = img.shape[1]
-	    			H = img.shape[0]
-	    			h = []
+					H = img.shape[0]
+					h = []
 	    			for l in range(L+1):
 	        			w_step = math.floor(W/(2**l))
 	        			h_step = math.floor(H/(2**l))
