97916089b1968b6efa96726301de476ac3abf7a0
marcocuccarini <59500902+marcocuccarini@users.noreply.github.com>
2022-01-20 23:27:56 +0100

    d

--- script/__init__.py
+++ script/__init__.py
@@ -30,8 +30,8 @@ class BagofWord():
 		        path = folder + "/" + filename
 		        for cat in os.listdir(path):
 		        	#load each image with a gray scale 
-		            img = cv2.imread(path + "/" + cat, 0)
-		            #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		            #img = cv2.imread(path + "/" + cat, 0)
+		            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 		            if img is not None:
 		                category.append(img)
 		        images[filename] = category
